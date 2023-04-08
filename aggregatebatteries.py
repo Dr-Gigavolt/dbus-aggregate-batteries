2e5ab13668af6ffce4960bad6797edaabd9d6147
Anton Labanc, PhD <115069923+Dr-Gigavolt@users.noreply.github.com>
2023-04-08 21:16:42 +0200

    Update aggregatebatteries.py
    Line 458, mix of tabs and spaces corrected.

--- aggregatebatteries.py
+++ aggregatebatteries.py
@@ -455,7 +455,7 @@ class DbusAggBatService(object):
                         Current_VE -= self._dbusMon.dbusmon.get_value(self._smartShunt, '/Dc/0/Current')
                         
                 if Current_VE is not None:
-	            Current = Current_VE                                                                            # BMS current overwritten only if no exception raised
+                    Current = Current_VE                                                                            # BMS current overwritten only if no exception raised
                     Power = Voltage * Current_VE                                                                    # calculate own power (not read from BMS)        
             
             except Exception:
