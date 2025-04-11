08cf63a705304afb424541856c2a7b772c099378
Anton Labanc, PhD. <115069923+Dr-Gigavolt@users.noreply.github.com>
2025-04-11 23:38:06 +0200

    Update aggregatebatteries.py
    Version 3.5

--- aggregatebatteries.py
+++ aggregatebatteries.py
@@ -31,7 +31,7 @@ from threading import Thread
 sys.path.append("/opt/victronenergy/dbus-systemcalc-py/ext/velib_python")
 from vedbus import VeDbusService  # noqa: E402
 
-VERSION = "3.41"
+VERSION = "3.5"
 
 class SystemBus(dbus.bus.BusConnection):
     def __new__(cls):
